d4b30cf1d8a8cc3364581696a2b7eb9249ba30fd
vserem <s223694903@deakin.edu.au>
2023-08-17 13:18:35 +1000

    indentation formatting

--- scripts.js
+++ scripts.js
@@ -1,60 +1,57 @@
-
 //function to add cardlist to html
 const addCards = (items) => {
-    items.forEach(item =>{
-    let itemToAppend = '<div class="col s4 center-align">'+
-        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
-        '</div><div class="card-content">'+
-        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-        '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-        '</div></div></div>';
-    $("#card-section").append(itemToAppend);
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.desciption + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend);
     });
-    }
+}
 
-    const formSubmitted = () => {
-        let formData = {};
-        formData.title = $('#title').val();
-        formData.subTitle = $('#subTitle').val();
-        formData.path = $('#path').val();
-        formData.description= $('#description').val();
+const formSubmitted = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
 
-        console.log(formData);
-        postCat(formData);
-        }
+    console.log(formData);
+    postCat(formData);
+}
 
 
-
-function postCat(cat){
+function postCat(cat) {
     $.ajax({
-        url:'/api/cat',
-        type:'POST',
-        data:cat,
-        success:(result)=>{
-            if(result.statuscode===201){
+        url: '/api/cat',
+        type: 'POST',
+        data: cat,
+        success: (result) => {
+            if (result.statuscode === 201) {
                 alert('cat post successful');
             }
         }
     })
 }
- 
-function getAllCats(){
-  $.get('/api/cats',(response)=>{
-    if (response.statuscode===200){
-        addCards(response.data);
-    }
-  }); 
+
+function getAllCats() {
+    $.get('/api/cats', (response) => {
+        if (response.statuscode === 200) {
+            addCards(response.data);
+        }
+    });
 }
 
-$(document).ready(function(){
-        $('.materialboxed').materialbox();
-        $('#formSubmit').click(()=>{
-            formSubmitted();
-        });
-        $('.modal').modal();
-        getAllCats();
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(() => {
+        formSubmitted();
+    });
+    $('.modal').modal();
+    getAllCats();
 
-    });    
-    
+});b
--- server.js
+++ server.js
@@ -1,71 +1,85 @@
-let express =require('express');
+let express = require('express');
 
-let app= express();
-const { MongoClient, ServerApiVersion, Collection } = require("mongodb");
-const uri="mongodb+srv://serem:bApIzgvwyKE2k5Rp@cluster0.5mtadof.mongodb.net/?retryWrites=true&w=majority"
+let app = express();
+const {
+    MongoClient,
+    ServerApiVersion,
+    Collection
+} = require("mongodb");
+const uri = "mongodb+srv://serem:bApIzgvwyKE2k5Rp@cluster0.5mtadof.mongodb.net/?retryWrites=true&w=majority"
 
 let port = process.env.port || 3000;
 let collection;
 
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({
+    extended: false
+}));
 
 
 //create a mongoClient with a mongoClient Options to set the stable api version
-const client= new MongoClient(uri,{
-    serverApi:{
+const client = new MongoClient(uri, {
+    serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
-        deprecationErrors: true, 
+        deprecationErrors: true,
     },
 });
 
-async function runDBconnect(){
+async function runDBconnect() {
     try {
         //connects the client to the server
         await client.connect();
-        collection=client.db().collection('Cat');
+        collection = client.db().collection('Cat');
         console.log(collection);
-    } catch(ex){
+    }
+    catch (ex) {
         console.error(ex);
     }
 }
 
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.get('/api/cats',(req,res)=>{
-    getAllCats((err,result)=>{
-        if(!err){
-            res.json({statuscode:200, data:result, message:'get all calls sucessful'});
-        } 
-    });
-});
-
-
-app.post('/api/cat',(req,res)=>{
-    let cat=req.body;
-    postCat(cat,(err,result)=>{
-        if(!err){
-            res.json({statuscode:201,data:result,message:'success'});
+app.get('/api/cats', (req, res) => {
+    getAllCats((err, result) => {
+        if (!err) {
+            res.json({
+                statuscode: 200,
+                data: result,
+                message: 'get all calls sucessful'
+            });
         }
     });
 });
 
-function postCat(cat,callback){
-    collection.insertOne(cat,callback);
+
+app.post('/api/cat', (req, res) => {
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({
+                statuscode: 201,
+                data: result,
+                message: 'success'
+            });
+        }
+    });
+});
+
+function postCat(cat, callback) {
+    collection.insertOne(cat, callback);
 }
 
-function getAllCats(callback){
+function getAllCats(callback) {
     collection.find({}).toArray(callback);
 }
 
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('server started');
     runDBconnect();
 });
