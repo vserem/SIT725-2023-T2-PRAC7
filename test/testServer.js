b02ff7046b9a9a3d922863532b67f842736f2d25
vserem <s223694903@deakin.edu.au>
2023-09-05 00:23:29 +1000

    Update testServer.js

--- test/testServer.js
+++ test/testServer.js
@@ -1,40 +1,42 @@
-const {expect}= require("chai");
-const request =require("request");
+const { expect, assert } = require("chai");
+const request = require("request");
 
-let url="http://localhost:3000/api/cat";
-let cat= {
-    title:'',
-    subTitle:'',
-    description:'',
-    path:''
+let url = "http://localhost:3000/api/cat";
+let cat = {
+    title: "Test Cat",
+    subTitle: "Mocha Testing Cat",
+    path: "images/kitten.jpg",
+    description: "My Testing Cat"
+
 }
 
 
-describe('test GET api',function(){
-    it('returns statuscode of 200', function(done){
-        request('http://localhost:3000/api/cat', function(error,response,bodyString){
-            let bodyObj =JSON.parse(bodyString);
+describe('test GET api', function () {
+    it('returns statuscode of 200', function (done) {
+        request('http://localhost:3000/api/cat', function (error, response, bodyString) {
+            let bodyObj = JSON.parse(bodyString);
+            expect(bodyObj.message).to.equal('get all calls sucessful');
+            done();
+        });
+    });
+});
+
+describe('test POST api', function () {
+    it('post cat to DB', function (done) {
+        request.post(url,{form: cat }, function (error, b, c) {
+            let bodyObj = JSON.parse(c);
             expect(bodyObj.message).to.equal('success');
             done();
         });
     });
 });
 
-describe('test POST api', function(){
-    it('post cat to DB',function(done){
-        request.post({url:'url',form:cat}, function(error,b,c){
-            let bodyObj= JSON.parse(c);
-            expect(bodyObj.massage).to.equal('success');
-            done();
-        });
-    });
-});
 
-
-describe('test DELETE api', function(){
-    it('delete a cat', function(done){
-        request.delete({url:'url', form:cat}, function(a,b,c){
-            ///todo asserts
+describe('test DELETE api', function () {
+    it('delete a cat', function (done) {
+        request.delete(url,{form: cat }, function (error, b, c) {
+            let bodyObj = JSON.parse(c);
+            assert.equal("success",bodyObj.message)
             done();
         });
     });
